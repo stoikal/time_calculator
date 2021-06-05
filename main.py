c9f8eb6a118f241c5bcf9cb2c7898a56023f9866
Laksito Pamilih <laksito@pm.me>
2021-06-05 15:11:58 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from time_calculator import add_time
+
+print(
+    add_time("3:00 PM", "3:10")
+    # Returns: 6:10 PM
+)
+
+print(
+    add_time("11:30 AM", "2:32", "Monday")
+    # Returns: 2:02 PM, Monday
+)
+
+print(
+    add_time("11:43 AM", "00:20")
+    # Returns: 12:03 PM
+)
+
+print(
+    add_time("10:10 PM", "3:30")
+    # Returns: 1:40 AM (next day)
+)
+
+print(
+    add_time("11:43 PM", "24:20", "tueSday")
+    # Returns: 12:03 AM, Thursday (2 days later)
+)
+
+print(
+    add_time("6:30 PM", "205:12") 
+    # Returns: 7:42 AM (9 days later)
+)
+
+# print(
+#     add_time("11:0 PM", "2:0")
+# )
+
+# print(
+#     add_time("11:0 AM", "1:0")
+# )
+
+# print(
+#     add_time("11:0 AM", "2:0")
+# )
---
+++ time_calculator.py
@@ -0,0 +1,48 @@
+def add_time(start, duration, starting_day=None):
+    days = ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday')
+    [start_time, start_period] = start.split()
+    [start_hour, start_minute] = start_time.split(':')
+    [duration_hours, duration_minutes] = duration.split(':')
+
+    start_hour = int(start_hour) + 12 if start_period == 'PM' and int(start_hour) < 12 else int(start_hour)
+    start_minute = int(start_minute)
+    duration_days = int(duration_hours) // 24
+    duration_hours = int(duration_hours) % 24
+    duration_minutes = int(duration_minutes)
+
+    result_day = duration_days
+    result_is_AM = start_period == 'AM'
+    result_hour = start_hour + duration_hours
+    result_minute = start_minute + duration_minutes
+
+    if result_minute >= 60 :
+        while result_minute >= 60 :
+            result_hour += 1
+            result_minute -= 60
+
+    if result_hour >= 24 :
+        while result_hour >= 24 :
+            result_day += 1
+            result_hour -= 24
+            result_is_AM = result_is_AM == False
+    elif result_hour >= 12 :
+        result_is_AM = False
+        if result_hour > 12 :
+            result_hour = result_hour % 12
+
+    result_period = 'AM' if result_is_AM else 'PM'
+    final_result_string = str(result_hour) + ':' + str(result_minute) + ' ' + result_period
+
+    if starting_day :
+        day_start = days.index(starting_day.lower())
+        day_name = days[(day_start + result_day) % len(days)].capitalize()
+
+        final_result_string += ', ' + day_name
+
+    if result_day :
+        if result_day > 1 :
+            final_result_string += ' (' + str(result_day) + ' days later)'
+        else :
+            final_result_string += ' (next day)'
+
+    return final_result_string
